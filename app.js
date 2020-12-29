c412102438e321239e64401343f2268147f37365
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-29 14:54:52 -0500

    added functionality for CRUD operations

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
+///////////Requests targeting all articles
+
+app.route("/articles")
+.get(function(req, res){
   Article.find(function(err, foundArticles){
     if (!err) {
       res.send(foundArticles);
@@ -29,16 +32,14 @@ app.get("/articles", function(req, res){
       res.send(err);
     }
   });
-});
-
-app.post("/articles", function(req, res){
-  console.log(req.body.title); 
-  console.log(req.body.content);
+})
+.post(function(req, res){
 
   const newArticle = new Article({
     title: req.body.title,
     content: req.body.content
   });
+
   newArticle.save(function(err){
     if (!err){
       res.send("Successfully added a new article");
@@ -46,7 +47,69 @@ app.post("/articles", function(req, res){
       res.send(err);
     }
   });
+})
+.delete(function(req, res){
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
 });
+
+///////////Requests targeting a specific article
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+  
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles mathing that title found")
+    }
+  });
+})
+.put(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      }
+    }
+  );
+})
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+})
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted the corresponding article.");
+      } else {
+        res.send(err);
+      }
+    }
+  );
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
